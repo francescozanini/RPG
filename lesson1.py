4285fcf01321270f694d235f9ae6df3dc5ebdb4b
francescozanini <zanini.francesco@hotmail.it>
2019-11-26 17:39:39 +0100

    .

--- lesson1.py
+++
@@ -1,8 +0,0 @@
-from RPG import *
-
-story = Story('Storia 1')
-options = Character.get_character_list()
-user_input = get_user_input(options, mssg='\nChoose character')
-player = Character.init_given_character(options[user_input], True)
-player.show_stats()
-update_story(story, player)
